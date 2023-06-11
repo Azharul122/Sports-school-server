838eaee18ae1cddf39374990157d4c6aebcc3fff
Azharul Islam <azharulip@gmail.com>
2023-06-11 20:03:54 +0600

    user to admin and insstructor form admin dashboard APIs making done

--- index.js
+++ index.js
@@ -118,8 +118,31 @@ async function run() {
       res.send(result)
     })
 
+    app.patch("/admin/users/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+    const updateDocument={
+      $set:{
+        role:"admin"
+      }
+    }
+      const result=await userCollecion.updateOne(query,updateDocument)
+      res.send(result)
+    })
     
+    app.patch("/instructor/users/:id",async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+    const updateDocument={
+      $set:{
+        role:"instructor"
+      }
+    }
+      const result=await userCollecion.updateOne(query,updateDocument)
+      res.send(result)
+    })
 
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
