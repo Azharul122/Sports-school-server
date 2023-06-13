6197bd9360d1a2fd64841d61abf34d692d96d876
Azharul Islam <azharulip@gmail.com>
2023-06-13 17:59:10 +0600

    Selcted classess API make using gmail filter functionality added done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const stripe=require('stripe')(process.env.PAYMENT_SECRET_KEY)
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -26,22 +27,22 @@ app.post('/jwt', (req, res) => {
   res.send({ token })
 })
 
-const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (!authorization) {
-    return res.status(401).send({ error: true, message: 'unauthorized access' });
-  }
-  // bearer token
-  const token = authorization.split(' ')[1];
+// const verifyJWT = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res.status(401).send({ error: true, message: 'unauthorized access' });
+//   }
+//   // bearer token
+//   const token = authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.DB_ACCESS_TOKEN, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ error: true, message: 'unauthorized access' })
-    }
-    req.decoded = decoded;
-    next();
-  })
-}
+//   jwt.verify(token, process.env.DB_ACCESS_TOKEN, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ error: true, message: 'unauthorized access' })
+//     }
+//     req.decoded = decoded;
+//     next();
+//   })
+// }
 
 app.get('/', (req, res) => {
   res.send("running")
@@ -96,10 +97,24 @@ async function run() {
     app.get('/users/:email', async (req, res) => {
       const toyId = req.params.email;
       const query = { email: toyId };
-      const result = await userCollecion.findOne(query);
+      const result = await userCollecion.find(query).toArray();
       res.send(result)
     })
 
+    app.post("/craete-payment-intent",async(req,res)=>{
+      const {price}=req.body;
+      const amount =price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+    
     app.get('/users/admin/:email', async (req, res) => {
       const toyId = req.params.email;
       const query = { email: toyId };
@@ -130,6 +145,13 @@ async function run() {
 
     app.get("/selected-classes", async (req, res) => {
       const result = await selectedcClassesCollecion.find().toArray();
+      res.send(result)
+    })
+
+    app.get("/selected-classes/email/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await selectedcClassesCollecion.find(query).toArray()
       res.send(result)
     })
 
@@ -181,7 +203,64 @@ async function run() {
       res.send(result)
     })
 
-    
+    app.put("/class/feadback/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const clas = req.body
+      const updateDocument = {
+        $set: {
+          feadBack: clas.feadBack
+        }
+
+      }
+      const result = await classesCollecion.updateOne(query, updateDocument)
+      res.send(result)
+    })
+    app.delete('/selected-classes/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await selectedcClassesCollecion.deleteOne(query)
+      res.send(result)
+    })
+
+    app.post("/classes", async (req, res) => {
+      const item = req.body
+      const result = await classesCollecion.insertOne(item)
+      res.send(result)
+    })
+
+
+    {/* avilableSheets,name,price,image,iname,iemail,feadBack:"",status:"pending" */ }
+
+    app.put('/classUpadte/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      console.log(id)
+      const options = { upsert: true }
+      const clas = req.body;
+      console.log(clas)
+      const updatedClas = {
+        $set: {
+          name: clas.name,
+          image: clas.image,
+          price: clas.price,
+          avilableSheets: clas.avilableSheets
+        }
+      }
+      const result = classesCollecion.updateOne(query, updatedClas);
+      res.send(result)
+
+    })
+
+
+    app.get('/clas/:id', async (req, res) => {
+      const toyId = req.params.id;
+      const query = { _id: new ObjectId(toyId) }
+      const result = await classesCollecion.findOne(query);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
