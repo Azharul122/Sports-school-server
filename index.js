e57281945023f25384d63f149725fb9098144af4
Azharul Islam <azharulip@gmail.com>
2023-06-14 23:20:06 +0600

    vercel essue fixed done

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const userCollecion = client.db("ass12DB").collection("users")
@@ -105,6 +105,7 @@ async function run() {
       const result = await paymentsCollection.find(query).toArray()
       res.send(result)
     })
+
     app.post("/payments", async (req, res) => {
       const payment = req.body
       const {itemId}=payment
@@ -276,7 +277,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
